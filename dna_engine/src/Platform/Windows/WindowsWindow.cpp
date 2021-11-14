ceeec3f2226a751dd6f0feeb6f886ead66eecf31
iiievan <iiievan@yandex.ru>
2021-11-14 22:55:39 +0300

    16. ImGui mouse and keyboard events added.

--- dna_engine/src/Platform/Windows/WindowsWindow.cpp
+++ dna_engine/src/Platform/Windows/WindowsWindow.cpp
@@ -107,6 +107,14 @@ namespace dna_engine {
 			}
 		});
 
+		glfwSetCharCallback(m_Window, [](GLFWwindow* window, unsigned int keycode)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			KeyTypedEvent event(keycode);
+			data.EventCallback(event);
+		});
+
 		glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods)
 		{
 			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
--- dna_engine/src/dna_engine/ImGui/ImGuiLayer.h
+++ dna_engine/src/dna_engine/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "dna_engine/Layer.h"
+#include "dna_engine/Events/MouseEvent.h"
+#include "dna_engine/Events/KeyEvent.h"
+#include "dna_engine/Events/ApplicationEvent.h"
 
 namespace dna_engine
 {
@@ -14,6 +17,17 @@ namespace dna_engine
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+
+		bool OnMouseButtonPressEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleaseEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizedEvent(WindowResizeEvent& e);
 
 	private:
 		float m_Time = 0.0f;
